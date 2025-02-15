dea37cc1e895ef5b08c1cf6a6a1a56a623ae4a2c
Shreyas Pradeepkumar Khandale <78716247+sherurox@users.noreply.github.com>
2025-02-15 17:11:46 -0500

    Add files via upload

---
+++ src/bloodbankmanagement/Bloodbankmanagement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bloodbankmanagement;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bloodbankmanagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+    }
+    
+}
